e93ad16db7033c77dbb1b113ffe0e78804dcecbc
FarahElshenawi <farah.elshenawy2@gmail.com>
2025-04-18 15:10:26 +0200

    Initial commit: Set up 100 Days of Code project

---
+++ Day1/main.py
@@ -0,0 +1,10 @@
+# Creating greeting for the program 
+print ("Welcome To The Brand Name Generators.")
+# Ask the user for the city they grew up in
+city_name= input("What's the name of the city you grew up in?\n")
+# Ask the user for the name of a pet 
+pet_name= input("What's your pet's name?\n")
+# Combine the name of their city and pet 
+brand_name= city_name + " " + pet_name
+# S how them their brand name
+print(f"Your brand name could be {brand_name}")
