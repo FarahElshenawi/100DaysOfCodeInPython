7238bb4657d861743fa291875506c22a9c438fa4
FarahElshenawi <farah.elshenawy2@gmail.com>
2025-04-19 11:14:01 +0200

    Edit output formatting to be 2 dicimal

--- Day02/final_project.py
+++ Day02/final_project.py
@@ -8,6 +8,7 @@ tip= int(input("How much tip would you like to give? 10, 12, or 15? "))
 no_of_people= int(input("How many people to split the bill? "))
 
 aLL_cost= total_pill + total_pill* (tip/100)
-cost_per_person= round(aLL_cost / no_of_people, 2)
+cost_per_person= aLL_cost / no_of_people
+final_amount= "{:.2f}".format(cost_per_person)
 
-print(f"Each person should pay: ${cost_per_person}")
+print(f"Each person should pay: ${final_amount}")
